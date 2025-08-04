1f1cb192049dd26c8c738b34735fc66a2b58d0be
mikera <mike@mikera.net>
2025-08-04 17:31:29 +0100

    More diagram edits

--- src/components/diagram/ConstNode.jsx
+++ src/components/diagram/ConstNode.jsx
@@ -5,16 +5,15 @@ function ConstNode({ data }) {
   console.log(data)
   return (
 
-    <div className="w-24 h-16 rounded-full bg-pink-200 border border-slate-600">
+    <div className="rounded-md bg-pink-200 border border-slate-600 p-2">
       <Handle
         type="source"
         position={Position.Right}
         id={data.id}
         className="!w-3 !h-3 !rounded-full !border-2 !bg-purple-400 !border-purple-400  "
       >
-        <div className="text-sm flex items-center text-center text-black -translate-x-20 ">{data.nodeLabel}</div>
       </Handle>
-
+      <div className="text-sm flex items-center text-center text-black">{data.nodeLabel}</div>
     </div>
   );
 }
