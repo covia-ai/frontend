7800cbdd26a8b11a48ce4070223d4dfa8f227e6b
anutechrepo77 <anu.dev.c09@gmail.com>
2025-10-08 10:02:46 +0530

    Call to action button in different colors

--- src/components/Iconbutton.tsx
+++ src/components/Iconbutton.tsx
@@ -29,14 +29,14 @@ export const Iconbutton = (props:any) => {
               (
               (props.pathId && props.pathId !="" ) ?
 
-              (<div className="inline-flex items-center justify-center border-1 border-slate-300 rounded-[11px] px-2 py-2 h-8 w-8 hover:bg-muted">
+              (<div className="inline-flex items-center justify-center border-1 border-slate-300 rounded-[11px] px-2 py-2 h-8 w-8 bg-primary text-white">
                   <Icon size={20} onClick={() => router.push("/venue/"+encodeURIComponent(venue.venueId)+"/"+props.path+"/" + props.pathId) }></Icon>
               </div>
               ):
               (
                 <div className="inline-flex items-center justify-center border-1 border-slate-300 rounded-[11px] px-2 py-2 h-8 w-8 hover:bg-muted">
                   <Icon size={20}></Icon> 
-              </div>
+                </div>
               )
               )
             }
